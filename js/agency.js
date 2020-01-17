a204c01d5daaeac509cb12e11cde42ea99b54051
jjaramil <jjosejaramillos@gmail.com>
2020-01-17 09:08:02 +0100

    added mail

--- js/agency.js
+++ js/agency.js
@@ -4,6 +4,85 @@
 }
 
 
+//index.js  
+function sendEmail() {
+
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var phone=$("#phone").val()
+  var message=$("#message").val();
+
+  const eeClient = require('elasticemail-webapiclient').client;
+
+const options = {
+    apiKey: '02FBD36CEDB72225CCCD7D854C0BD286C9364C902B3A14E28AE5988CFA70BB2565898CF02C16EC9FA73C5A295F87F3F3',
+    apiUri: 'https://api.elasticemail.com/',
+    apiVersion: 'v2'
+}
+
+const EE = new eeClient(options);
+
+// Load account data
+EE.Account.Load().then(function(resp) {
+    console.log(resp);
+});
+
+const emailParams = {
+    "subject":'Nuevo contacto en Fofasta',
+    "to": 'fasteano@fasta.es',
+    "from": 'jjosejaramillos@gmail.com',
+    "replyTo": 'fofasta@fasta.org',
+    "body": name + ' ' + email+ ' '+phone+' '+message ,
+    "fromName": 'FoFasta',
+    "bodyType": 'Plain'
+};
+
+// Send email
+EE.Email.Send(emailParams).Then(
+  message => alert("mail Sent")
+)
+.catch((err) => {
+    throw new Error(err);
+});
+
+//   jQuery.post("https://api.elasticemail.com/v2/contact/add", {
+//     email: $("#email").val(),
+//     publicAccountId: "fofastaSmtp",
+//     listName: "Jose"
+// }, function () { }, "json").done(function (result) {
+//     if (result.success == true) {
+//         onSuccess();
+//     }
+// }).fail(function () {
+//     // Do something here if it fails...
+// });
+
+//   var message = $("#name").val()+"  "+$("#email").val()+"  "+$("#phone").val()+" "+$("#message").val();
+//   Email.send("fasteano@fasta.es", 
+// "fasteano@fasta.es",           
+// "subject of mail",              
+// "message",                
+// "smtp.elasticemail.com",                 
+// "jjosejaramillos@gmail.com",         
+// "274B9B3A7F892B2198FDA4569DEE6C38F3BA").then(
+//   message => alert("mail sent successfully")
+// );   
+
+// 	Email.send({
+// 	Host: "smtp.elasticemail.com",
+// 	Username : "jjosejaramillos@gmail.com",
+//   Password : "274B9B3A7F892B2198FDA4569DEE6C38F3BA",
+//   Port:"2525",
+// 	To : 'fasteano@fasta.es',
+// 	From : "fasteano@fasta.es",
+// 	Subject : "Nuevo mensaje en Fofasta",
+// 	Body : message,
+// 	}).then(
+// 		message => alert("mail sent successfully")
+// 	);
+}
+
+
 (function($) {
   $('.carousel').carousel({
     interval: 3000
@@ -24,6 +103,8 @@
       }
     }
   });
+
+
 
 
   
