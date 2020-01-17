5105a6bda286b53c97dce429afb964c11793011e
jjaramil <jjosejaramillos@gmail.com>
2020-01-17 09:33:14 +0100

    mails

--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,5 @@
 
- function donate(){
+function donate() {
   $("#paypal-pay").click();
 }
 
@@ -9,81 +9,58 @@ function sendEmail() {
 
   var name = $("#name").val();
   var email = $("#email").val();
-  var phone=$("#phone").val()
-  var message=$("#message").val();
+  var phone = $("#phone").val()
+  var message = $("#message").val();
 
-  const eeClient = require('elasticemail-webapiclient').client;
+  Email.send({
+    Host : 'smtp.elasticemail.com',
+    Username : 'jjosejaramillos@gmail.com',
+    Password : '274B9B3A7F892B2198FDA4569DEE6C38F3BA',
+    To : 'jjosejaramillos@gmail.com',
+    From : 'jjosejaramillos@gmail.com',
+    Subject :'nuevo mensaje de Fofasta',
+    Body : "<i>This is sent as a feedback from my resume page.</i> <br/> <b>Name: </b>"+name+" <br/> <b>Email: </b>"+email+"<br /> <b>Phone: </b>"+phone+"<br /> <b>Message:</b> <br /> "+message+" <br><br> <b>~End of Message.~</b> "
+    }).then( message => {alert(message) } );
 
-const options = {
-    apiKey: '02FBD36CEDB72225CCCD7D854C0BD286C9364C902B3A14E28AE5988CFA70BB2565898CF02C16EC9FA73C5A295F87F3F3',
-    apiUri: 'https://api.elasticemail.com/',
-    apiVersion: 'v2'
-}
+  //   jQuery.post("https://api.elasticemail.com/v2/contact/add", {
+  //     email: $("#email").val(),
+  //     publicAccountId: "fofastaSmtp",
+  //     listName: "Jose"
+  // }, function () { }, "json").done(function (result) {
+  //     if (result.success == true) {
+  //         onSuccess();
+  //     }
+  // }).fail(function () {
+  //     // Do something here if it fails...
+  // });
 
-const EE = new eeClient(options);
+  //   var message = $("#name").val()+"  "+$("#email").val()+"  "+$("#phone").val()+" "+$("#message").val();
+  //   Email.send("fasteano@fasta.es", 
+  // "fasteano@fasta.es",           
+  // "subject of mail",              
+  // "message",                
+  // "smtp.elasticemail.com",                 
+  // "jjosejaramillos@gmail.com",         
+  // "274B9B3A7F892B2198FDA4569DEE6C38F3BA").then(
+  //   message => alert("mail sent successfully")
+  // );   
 
-// Load account data
-EE.Account.Load().then(function(resp) {
-    console.log(resp);
-});
-
-const emailParams = {
-    "subject":'Nuevo contacto en Fofasta',
-    "to": 'fasteano@fasta.es',
-    "from": 'jjosejaramillos@gmail.com',
-    "replyTo": 'fofasta@fasta.org',
-    "body": name + ' ' + email+ ' '+phone+' '+message ,
-    "fromName": 'FoFasta',
-    "bodyType": 'Plain'
-};
-
-// Send email
-EE.Email.Send(emailParams).Then(
-  message => alert("mail Sent")
-)
-.catch((err) => {
-    throw new Error(err);
-});
-
-//   jQuery.post("https://api.elasticemail.com/v2/contact/add", {
-//     email: $("#email").val(),
-//     publicAccountId: "fofastaSmtp",
-//     listName: "Jose"
-// }, function () { }, "json").done(function (result) {
-//     if (result.success == true) {
-//         onSuccess();
-//     }
-// }).fail(function () {
-//     // Do something here if it fails...
-// });
-
-//   var message = $("#name").val()+"  "+$("#email").val()+"  "+$("#phone").val()+" "+$("#message").val();
-//   Email.send("fasteano@fasta.es", 
-// "fasteano@fasta.es",           
-// "subject of mail",              
-// "message",                
-// "smtp.elasticemail.com",                 
-// "jjosejaramillos@gmail.com",         
-// "274B9B3A7F892B2198FDA4569DEE6C38F3BA").then(
-//   message => alert("mail sent successfully")
-// );   
-
-// 	Email.send({
-// 	Host: "smtp.elasticemail.com",
-// 	Username : "jjosejaramillos@gmail.com",
-//   Password : "274B9B3A7F892B2198FDA4569DEE6C38F3BA",
-//   Port:"2525",
-// 	To : 'fasteano@fasta.es',
-// 	From : "fasteano@fasta.es",
-// 	Subject : "Nuevo mensaje en Fofasta",
-// 	Body : message,
-// 	}).then(
-// 		message => alert("mail sent successfully")
-// 	);
+  // 	Email.send({
+  // 	Host: "smtp.elasticemail.com",
+  // 	Username : "jjosejaramillos@gmail.com",
+  //   Password : "274B9B3A7F892B2198FDA4569DEE6C38F3BA",
+  //   Port:"2525",
+  // 	To : 'fasteano@fasta.es',
+  // 	From : "fasteano@fasta.es",
+  // 	Subject : "Nuevo mensaje en Fofasta",
+  // 	Body : message,
+  // 	}).then(
+  // 		message => alert("mail sent successfully")
+  // 	);
 }
 
 
-(function($) {
+(function ($) {
   $('.carousel').carousel({
     interval: 3000
   })
@@ -91,7 +68,7 @@ EE.Email.Send(emailParams).Then(
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -107,9 +84,9 @@ EE.Email.Send(emailParams).Then(
 
 
 
-  
+
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -120,7 +97,7 @@ EE.Email.Send(emailParams).Then(
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
