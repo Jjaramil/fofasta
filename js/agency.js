85b9ce627d3e4cbe1d487e291d35d48e5dc676e3
jjaramil <jjosejaramillos@gmail.com>
2020-02-28 12:03:29 +0100

    added payment methods

--- js/agency.js
+++ js/agency.js
@@ -4,7 +4,36 @@ function donate() {
 }
 
 
-    
+function checkDonationAmount(elem){
+  if($(elem).val() > 0 ){
+    $(".disabled-overlay").removeClass("activate")
+  }else{
+    $(".disabled-overlay").addClass("activate")
+  }
+}
+
+
+paypal.Buttons({
+  createOrder: function(data, actions) {
+    var amount = $("#donation-amount").val();
+    // This function sets up the details of the transaction, including the amount and line item details.
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: amount
+        }
+      }]
+    });
+  },
+  onApprove: function(data, actions) {
+    // This function captures the funds from the transaction.
+    return actions.order.capture().then(function(details) {
+      // This function shows a transaction success message to your buyer.
+      alert('Transaction completed by ' + details.payer.name.given_name);
+    });
+  }
+}).render('#pp-buttons-container');
+
 
 //index.js  
 function sendEmail() {
